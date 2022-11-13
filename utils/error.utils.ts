dc26e04ed5877b85fb904602c5ff80da34ed30f3
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-13 19:00:51 +0700

    refactor: remove return status key in transfomAxiosError util

--- utils/error.utils.ts
+++ utils/error.utils.ts
@@ -1,8 +1,7 @@
 export function uTranformAxiosError(error: any) {
   return {
-    status: "ERROR",
     name: error.response?.data?.name || "UNKNOWM_ERROR",
     message: error.response?.data?.message || "Something wrong",
-    statusCode: error.response?.data.statusCode || 500,
+    statusCode: error.response?.data?.statusCode || 500,
   };
 }

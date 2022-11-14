eea02e438ab41826c1addb15b8a92d15a558b761
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-14 23:32:07 +0700

    feat: companies component

---
+++ components/organisms/companies/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+export default function Companies({}: Props) {
+  return (
+    <section className="py-5">
+      <div className="d-none d-lg-flex justify-content-center   mt-70 hero-logos">
+        <img src="/svg/storegg-x-companies.svg" alt="StoreGG" />
+      </div>
+    </section>
+  );
+}

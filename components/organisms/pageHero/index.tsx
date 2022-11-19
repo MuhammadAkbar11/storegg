ffe91cff41c06c4b52dc2991b9eabfe6bbdb5e9e
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-20 06:15:59 +0700

    refactor: using classnames in pageHero component

--- components/organisms/pageHero/index.tsx
+++ components/organisms/pageHero/index.tsx
@@ -1,17 +1,29 @@
 import React from "react";
+import clsx from "classnames";
 
 type Props = {
   title: React.ReactNode | string;
   text: React.ReactNode | string;
+  align?: "start" | "center";
 };
 
-function PageHero({ title, text }: Props) {
+function PageHero({ title, text, align }: Props) {
+  const rowClassName = clsx("row", {
+    "justify-content-center": align === "center",
+    "justify-content-start": align === "start",
+  });
+
+  const colClassName = clsx("col-md-6 col-lg-4 col-11", {
+    "text-center": align === "center",
+    "text-start": align === "start",
+  });
+
   return (
-    <section className="pt-5 pb-4 ">
+    <section className="pt-5 pb-4 hero ">
       <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-lg-12 col-11 text-center">
-            <h1 className="text-5xl fw-bold color-palette-1 mb-1 lh-base">
+        <div className={rowClassName}>
+          <div className={colClassName}>
+            <h1 className="hero-title fw-bold color-palette-1 mb-1 lh-base">
               {title}
             </h1>
             <p className="text-md color-palette-6">{text}</p>
@@ -25,6 +37,7 @@ function PageHero({ title, text }: Props) {
 PageHero.defaultProps = {
   title: "Page Hero",
   text: "We provide millions of ways to help players become real winners.",
+  align: "center",
 };
 
 export default PageHero;

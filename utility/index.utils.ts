fc2024bac98fd1051a57ada3f7fae592b7588b4f
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-30 08:04:59 +0700

    fix: Filter query key values that are undefined, null, or empty. 

--- utility/index.utils.ts
+++ utility/index.utils.ts
@@ -1,11 +1,12 @@
 export function queriesToString<T extends Record<string, any>>(queries: T) {
+  Object.keys(queries).forEach(key => {
+    if (queries[key] === null) delete queries[key];
+    if (queries[key] === undefined) delete queries[key];
+    if (typeof queries[key] === "string" && queries[key].trim() === "")
+      delete queries[key];
+  });
+
   return Object.keys(queries)
-    .filter(
-      key =>
-        queries[key] !== null ||
-        queries[key] !== undefined ||
-        queries[key]?.trim() !== ""
-    )
     .map(key => {
       return `${key}=${queries[key]}`;
     })

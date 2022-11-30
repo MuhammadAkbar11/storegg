d113c4793af3e239dc398fc1d728491bd7a4bcaf
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-30 08:16:45 +0700

    feat: use react-query on the FeaturedGames component

--- components/organisms/featuredGames/index.tsx
+++ components/organisms/featuredGames/index.tsx
@@ -1,39 +1,18 @@
 import React from "react";
 import { getFeaturedGameService } from "@services/player.service";
 import GameItem from "@molecules/gameItem";
-import { IGameItem } from "@utility/types";
+import { useQuery } from "react-query";
 
 type Props = {};
 
 function FeaturedGames({}: Props) {
-  const effRun = React.useRef(false);
-  const [loadingFeaturedGames, setLoadingFeatured] =
-    React.useState<boolean>(true);
-  const [featuredGames, setFeaturedGames] = React.useState<IGameItem[]>([]);
-
-  const onGetFeaturedGameList = React.useCallback(async () => {
-    try {
-      const games = await getFeaturedGameService({
-        limit: 5,
-        page: 1,
-        sortBy: "featured",
-      });
-
-      setFeaturedGames(games);
-      setLoadingFeatured(false);
-    } catch (error) {
-      console.log(error);
-    }
-  }, [getFeaturedGameService]);
-
-  React.useEffect(() => {
-    if (effRun.current === true) {
-      onGetFeaturedGameList();
-    }
-    return () => {
-      effRun.current = true;
-    };
-  }, [effRun]);
+  const {
+    data: featuredGames,
+    isLoading,
+    isSuccess,
+  } = useQuery("featuredGames", () =>
+    getFeaturedGameService({ limit: 5, page: 1, sortBy: "featured" })
+  );
 
   return (
     <section className="featured-game pt-50 pb-50">
@@ -42,7 +21,7 @@ function FeaturedGames({}: Props) {
           Our Featured
           <br /> Games This Year
         </h2>
-        {loadingFeaturedGames ? (
+        {isLoading ? (
           <div className=" w-full d-flex justify-content-center py-5 ">
             <div className="spinner-border color-palette-4" role="status">
               <span className="visually-hidden">Loading...</span>
@@ -53,7 +32,7 @@ function FeaturedGames({}: Props) {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-start gap-lg-3 gap-4 pb-3 "
           data-aos="fade-up"
         >
-          {featuredGames.length !== 0
+          {isSuccess
             ? featuredGames?.map(gm => {
                 return (
                   <GameItem

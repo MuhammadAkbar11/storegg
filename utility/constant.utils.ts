cbd7e89bdf667c5a87d66deb6510eb2bb0b8fb98
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-12-04 08:30:24 +0700

    feat: adding a constant for the profile menu

--- utility/constant.utils.ts
+++ utility/constant.utils.ts
@@ -1,3 +1,10 @@
 export const UNKNOWM_ERROR = "UNKNOWM_ERROR";
 export const ROOT_API = process.env.NEXT_PUBLIC_API_URI;
 export const API_URI = `${process.env.NEXT_PUBLIC_API_URI}${process.env.NEXT_PUBLIC_API_PATH}`;
+
+export const PROFILE_MENU = [
+  { text: "My Profile", href: "/member" },
+  { text: "Wallet", href: "/member/wallet" },
+  { text: "Account Setting", href: "/member/settings" },
+  { text: "Log Out", href: "/auth/sing-in" },
+];

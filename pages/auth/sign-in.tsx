518f182f4c49e0cfc8e84f91a9380d3cff0b7ee5
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-12-05 07:43:07 +0700

    refactor: implementation of the component Layout on the login page

--- pages/auth/sign-in.tsx
+++ pages/auth/sign-in.tsx
@@ -3,12 +3,13 @@ import React from "react";
 import Logo from "@atoms/logo";
 import SignInCover from "@organisms/auth/signInCover";
 import SignInForm from "@organisms/auth/signInForm";
+import Layout from "@components/organisms/layout";
 
 type Props = {};
 
 function SignIn({}: Props) {
   return (
-    <>
+    <Layout navbar={false} footer={false} pageTitle="Sign in">
       <section className="sign-in mx-auto">
         <div className="row">
           <div className="col-xxl-5 col-lg-6 my-auto py-lg-0 pt-lg-50 pb-lg-50 pt-30 pb-47 px-0">
@@ -26,12 +27,12 @@ function SignIn({}: Props) {
               </div>
             </form>
           </div>
-          <div className="col-xxl-7 col-lg-6 bg-blue text-center pt-lg-145 pb-lg-145 d-lg-block d-none">
+          <div className="col-xxl-7 col-lg-6 bg-palette-blue text-center pt-lg-145 pb-lg-145 d-lg-block d-none">
             <SignInCover />
           </div>
         </div>
       </section>
-    </>
+    </Layout>
   );
 }
 

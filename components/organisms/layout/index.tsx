13ae1e2d33ed4894af4cbea9684fb6d0aa8418e5
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-12-08 02:18:26 +0700

    styles: import order in Layout comp

--- components/organisms/layout/index.tsx
+++ components/organisms/layout/index.tsx
@@ -1,8 +1,8 @@
-import Footer from "@components/organisms/footer";
-import NavbarMenu from "@components/organisms/navbarMenu";
+import React from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import React from "react";
+import Footer from "@components/organisms/footer";
+import NavbarMenu from "@components/organisms/navbarMenu";
 
 type Props = {
   pageTitle?: string;

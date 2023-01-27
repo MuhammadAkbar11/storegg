afeca4db5a5ca8e8ad688abfb10b7462137a307f
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-01-27 21:05:15 +0700

    fix: distructuring rest axios's error

--- utility/error.utils.ts
+++ utility/error.utils.ts
@@ -3,6 +3,6 @@ export function uTranformAxiosError(error: any) {
     name: error.response?.data?.name || "UNKNOWM_ERROR",
     message: error.response?.data?.message || "Something wrong",
     statusCode: error.response?.data?.statusCode || 500,
-    ...error.response.data.data,
+    ...error.response?.data?.data,
   };
 }

af3bc0c6168988ac9bd76642135c7eaebf8c03fd
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-02-14 19:46:50 +0700

    feat: finish useAuth hooks

--- utility/hooks/useAuth.tsx
+++ utility/hooks/useAuth.tsx
@@ -1,9 +1,46 @@
-import { getAuthService } from "@services/auth.service";
+import { getAuthService, getUserTokenService } from "@services/auth.service";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 
-const useAuth = () =>
-  useQuery("userAuth", getAuthService, {
-    staleTime: 15 * 1000,
+const useAuth = () => {
+  const token = getUserTokenService();
+
+  const queryAuth = useQuery("userAuth", () => getAuthService(token || ""), {
+    staleTime: 5 * 1000,
+    retry(failureCount: number, error: any) {
+      if (error) {
+        return false;
+      }
+      return true;
+    },
+    onError(err) {
+      if (err.name === "NOT_AUTH") {
+        return null;
+      }
+    },
+    useErrorBoundary(_error, _query) {
+      return false;
+    },
   });
+
+  const refetchAuth = () => queryAuth.refetch();
+
+  if (!queryAuth.isLoading && !queryAuth.data) {
+    return {
+      isAuth: false,
+      authState: null,
+      isLoading: queryAuth.isLoading,
+      refetchAuth,
+    };
+  }
+
+  return {
+    isAuth: !!queryAuth.data,
+    authState: queryAuth.data,
+    isLoading: queryAuth.isLoading,
+    refetchAuth,
+  };
+};
 
 export default useAuth;

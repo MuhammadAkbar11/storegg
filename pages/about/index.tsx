d29b86ed02edcb6cafc0d68e3aca36692d3da7a6
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2026-04-21 20:59:46 +0700

    chore: add meta description to enhance SEO for the Document component

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,11 +1,8 @@
 import AboutStory from "@components/organisms/aboutStory";
 import Companies from "@components/organisms/companies";
-import Footer from "@components/organisms/footer";
 import Layout from "@components/organisms/layout";
-import NavbarMenu from "@components/organisms/navbarMenu";
 import OurServices from "@components/organisms/ourServices";
 import PageHero from "@components/organisms/pageHero";
-import Head from "next/head";
 import React from "react";
 import useInitAOS from "utility/hooks/useInitAOS";
 
